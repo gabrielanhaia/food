e5ab1ffab64d7e317bb4c2ccbb1852ebb15d79ff
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2019-12-18 20:51:09 +0000

    feature: Implement endpoint to list products available

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -19,5 +19,12 @@ Route::group([
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    Route::get('me', 'AuthController@me');
+});
+
+
+Route::group([
+    'prefix' => 'products',
+], function ($router) {
+    Route::get('', 'ProductController@listProducts');
 });
